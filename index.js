8a6812c2ac57121855eec5f0fc0cb701082b6b84
Mahadi90 <mahadimhs787@gmail.com>
2023-05-02 02:14:33 +0600

    chef data send

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const chef = require('./data/chefAndRecipe.json')
+
 app.get('/', (req, res) => {
     res.send('Terrific chef  is running')
 });
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
 
 app.listen(port, () => {
     console.log(`Terrific chef API is running on port: ${port}`)
